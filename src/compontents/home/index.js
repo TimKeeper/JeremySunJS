e84d070dcf0d8772951117e86da9bdb78c469e23
JeremySun <jeremysun@JeremySundeMacBook-Pro.local>
2019-04-19 17:17:24 +0800

    react 16 route

---
+++ src/compontents/home/index.js
@@ -0,0 +1,14 @@
+import React, { PureComponent } from 'react'
+// import {Layout, Button} from 'antd'
+
+
+export default class Home extends PureComponent {
+	render () {
+		return (
+			<div>
+				{/* <Button type="primary">Button</Button> */}
+				asd
+			</div>
+		)
+	}
+}
---
+++ src/router/router.js
@@ -0,0 +1,21 @@
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import React, { PureComponent, Suspense, lazy } from 'react'
+const Home = lazy(() => import('../compontents/home/index'))
+// const About = lazy(() => import('./routes/About'))
+
+
+
+export default class AppRouter extends PureComponent {
+	render () {
+		return (
+			<Router>
+				<Suspense fallback={<div>Loading...</div>}>
+					<Switch>
+						<Route exact path="/" component={Home}/>
+						{/* <Route path="/about" component={About}/> */}
+					</Switch>
+				</Suspense>
+			</Router>
+		)
+	}
+}
